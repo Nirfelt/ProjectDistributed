950a46ea4552164e2b618c7f97f6f369e1c8815b
Sebastian Nirfelt <sebastian.nirfelt@gmail.com>
2015-03-16 16:47:11 +0100

    Fixed upload in masterConnection.go

--- go/master/masterConnection.go
+++ go/master/masterConnection.go
@@ -2,33 +2,30 @@ package main
 
 import (
 	"fmt"
-	//"io/ioutil"
-	"net/http"
 	"github.com/gorilla/mux"
-	//"os"
-	//"net/url"
-	//"bytes"
+	"net/http"
+	"io/ioutil"
+	"bytes"
 	"flag"
 	"log"
-	//"mime/multipart"
-	"unicode"
 	"strings"
+	"unicode"
 )
 
-type dataNodes struct{
+type dataNodes struct {
 	node []node
 }
 
-type node struct{
+type node struct {
 	address string
-	ok bool
+	ok      bool
 }
 
 var nodes = dataNodes{} // List with dataNodes (struct)
 
 var (
 	listen = flag.String("listen", "localhost:8080", "listen on address")
-	logp = flag.Bool("log", false, "enable logging")
+	logp   = flag.Bool("log", false, "enable logging")
 )
 
 func main() {
@@ -36,29 +33,23 @@ func main() {
 	flag.Parse()
 	AddDataNode("localhost:8081")
 	AddDataNode("localhost:8082")
-	//r := mux.NewRouter()
-	//update := r.Path("/update")//.Subrouter()
-	//update.Methods("POST").HandlerFunc(FileUploadHandler)
-	proxyHandler := http.HandlerFunc(proxyHandlerFunc)
-	log.Fatal(http.ListenAndServe(*listen, proxyHandler))
-
-	//err := http.ListenAndServe(":8080", r)
-	//if err != nil{
-	//	log.Fatal("ListenAndServe: ", err)
-	//}
+	r := mux.NewRouter()
+	update := r.Path("/update")
+	update.Methods("POST").HandlerFunc(proxyHandlerFunc)
+	http.ListenAndServe(":8080", r)
 }
 
-func AddDataNode(address string){
+func AddDataNode(address string) {
 	node := node{address: address, ok: true}
 	nodes.node = append(nodes.node, node)
 	//update DB
 }
 
-func RemoveDataNode(node string){
+func RemoveDataNode(node string) {
 	if len(nodes.node) == 0 {
 		return
 	}
-	for i := range nodes.node{
+	for i := range nodes.node {
 		if nodes.node[i].address == node {
 			nodes.node[i] = nodes.node[len(nodes.node)-1]
 			nodes.node = nodes.node[:len(nodes.node)-1]
@@ -69,22 +60,25 @@ func RemoveDataNode(node string){
 
 func proxyHandlerFunc(rw http.ResponseWriter, r *http.Request) {
 	output := ""
+	body, _ := ioutil.ReadAll(r.Body)
+
+	// Loop over all data nodes
 	for i := 0; i < len(nodes.node); i++ {
 		u := "http://" + nodes.node[i].address + "/update"
-	    req, err := http.NewRequest("POST", u, nil)
-	    if err != nil {
-			log.Fatal(err)
-			fmt.Fprintf(rw, "ERROR: Making request" + u)
-		}
-	    req.Body = r.Body
-	    req.Header = r.Header
-		req.URL.Scheme = strings.Map(unicode.ToLower, req.URL.Scheme)
+		reader := bytes.NewReader(body)
 
+		req, err := http.NewRequest("POST", u, ioutil.NopCloser(reader))
+		if err != nil {
+			log.Fatal(err)
+			fmt.Println(rw, "ERROR: Making request"+u)
+		}
+		req.Header = r.Header
+		req.URL.Scheme = strings.Map(unicode.ToLower, req.URL.Scheme)
 		client := &http.Client{}
 		resp, err := client.Do(req)
 		if err != nil {
 			log.Fatal(err)
-			fmt.Fprintf(rw, "ERROR: Sending request" + u)
+			fmt.Println(rw, "ERROR: Sending request"+u)
 		}
 		output = output + u + "\nStatus: " + resp.Status + "\nProtocol: " + resp.Proto + "\n\n"
 	}
@@ -110,6 +104,6 @@ func FileDeleteHandler(rw http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(rw, "Deleted file with id: %s, faculty: %s, course: %s, year: %s", id, faculty, course, year)
 }
 
-//func get datanode ip 
+//func get datanode ip
 
 //func return all files and folders
