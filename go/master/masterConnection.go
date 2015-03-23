f215d5807b956c5b4a17cbc70550eac101310b64
Sebastian Nirfelt <sebastian.nirfelt@gmail.com>
2015-03-23 11:32:45 +0100

    Added get_file to masteConnection, not tested

--- go/master/masterConnection.go
+++ go/master/masterConnection.go
@@ -6,11 +6,12 @@ import (
 	"fmt"
 	"github.com/gorilla/mux"
 	"io/ioutil"
-	"log"
+	//"log"
 	"net/http"
 	"os"
 	"strings"
 	"unicode"
+	"encoding/json"
 )
 
 type dataNodes struct {
@@ -46,8 +47,114 @@ func main() {
 	deleteFile := r.Path("/delete/{id}")
 	deleteFile.Methods("DELETE").HandlerFunc(FileDeleteHandler)
 
+	getFile := r.Path("/get_file/{id}")
+	getFile.Methods("GET").HandlerFunc(GetFileHandler)
+
 	NotifyRouter()
 	http.ListenAndServe(":"+os.Getenv("PORT"), r)
+}
+
+
+func GetFileHandler(rw http.ResponseWriter, r *http.Request) {
+	//Get dataNode address from masterDB
+	id := mux.Vars(r)["id"]
+	resp, err := http.Get("http://" + masterDB + "/get_server/" + id)
+	if err != nil {
+		fmt.Println("ERROR: Sending request"+masterDB)
+	}
+	//Handle response
+	defer resp.Body.Close()
+	body, _ := ioutil.ReadAll(resp.Body)
+	var data []string
+	err = json.Unmarshal(body, &data)
+	if err != nil {
+		fmt.Println("ERROR: Unmarshall json")
+	}
+	for i := 0; i < len(data); i++ {
+		fmt.Println(data[i])
+	}
+}
+
+func ProxyHandlerFunc(rw http.ResponseWriter, r *http.Request) {
+	output := ""
+	body, _ := ioutil.ReadAll(r.Body)
+
+	// Loop over all data nodes
+	for i := 0; i < len(nodes.node); i++ {
+		u := "http://" + nodes.node[i].address + "/update"
+		reader := bytes.NewReader(body)
+
+		req, err := http.NewRequest("POST", u, ioutil.NopCloser(reader))
+		if err != nil {
+			fmt.Println(rw, "ERROR: Making request"+u)
+		}
+		req.Header = r.Header
+		req.URL.Scheme = strings.Map(unicode.ToLower, req.URL.Scheme)
+		client := &http.Client{}
+		resp, err := client.Do(req)
+		if err != nil {
+			fmt.Println(rw, "ERROR: Sending request"+u)
+		}
+		output = output + u + "\nStatus: " + resp.Status + "\nProtocol: " + resp.Proto + "\n\n"
+	}
+	fmt.Println(output)
+	fmt.Fprintf(rw, output)
+}
+
+//Func for multicasting id of file to delete to nodes
+func FileDeleteHandler(rw http.ResponseWriter, r *http.Request) {
+	fmt.Println("master ok")
+	id := mux.Vars(r)["id"] //Get file id from request path
+	output := ""
+
+	// Loop over all data nodes
+	for i := 0; i < len(nodes.node); i++ {
+		u := "http://" + nodes.node[i].address + "/delete/" + id //Specific url for every node
+
+		req, err := http.NewRequest("DELETE", u, nil) //Create new request
+		if err != nil {
+			fmt.Println(rw, "ERROR: Making request"+u)
+		}
+		req.Header = r.Header
+		req.URL.Scheme = strings.Map(unicode.ToLower, req.URL.Scheme)
+
+		client := &http.Client{}
+		resp, err := client.Do(req) //Send request, get response
+		if err != nil {
+			fmt.Println(rw, "ERROR: Sending request"+u)
+		}
+		output = output + u + "\nStatus: " + resp.Status + "\nProtocol: " + resp.Proto + "\n\n" //Output string
+	}
+	fmt.Println(output)
+	fmt.Fprintf(rw, output)
+}
+
+//Handles new datanodes connecting
+func HandshakeHandler(rw http.ResponseWriter, r *http.Request) {
+	handshake := mux.Vars(r)["nodeAddress"]
+	AddDataNode(handshake)
+
+	fmt.Println("Handshake: " + handshake)
+}
+
+func NotifyRouter() {
+	masterAddress := "localhost:" + os.Getenv("PORT")
+
+	url := "http://" + routerAddress + "/handshake/" + masterAddress
+	r, err := http.NewRequest("POST", url, nil)
+	if err != nil {
+		fmt.Printf("ERROR: Making request" + url)
+	}
+
+	client := &http.Client{}
+	resp, err := client.Do(r)
+
+	if err != nil {
+		fmt.Printf("ERROR: Sending request" + url)
+	}
+	output := url + "\nStatus: " + resp.Status + "\nProtocol: " + resp.Proto + "\n\n"
+
+	fmt.Println(output)
 }
 
 func AddDataNode(address string) {
@@ -58,13 +165,11 @@ func AddDataNode(address string) {
 
 	req, err := http.NewRequest("PUT", u, nil) //Create new request
 	if err != nil {
-		log.Fatal(err)
 		fmt.Println("ERROR: Making request"+u)
 	}
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
-		log.Fatal(err)
 		fmt.Println("ERROR: Sending request"+u)
 	}
 	fmt.Println(u + "\nStatus: " + resp.Status + "\nProtocol: " + resp.Proto + "\n\n")
@@ -85,104 +190,14 @@ func RemoveDataNode(node string) {
 
 	req, err := http.NewRequest("DELETE", u, nil) //Create new request
 	if err != nil {
-		log.Fatal(err)
 		fmt.Println("ERROR: Making request"+u)
 	}
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
-		log.Fatal(err)
 		fmt.Println("ERROR: Sending request"+u)
 	}
 	fmt.Println(u + "\nStatus: " + resp.Status + "\nProtocol: " + resp.Proto + "\n\n")
-}
-
-func ProxyHandlerFunc(rw http.ResponseWriter, r *http.Request) {
-	output := ""
-	body, _ := ioutil.ReadAll(r.Body)
-
-	// Loop over all data nodes
-	for i := 0; i < len(nodes.node); i++ {
-		u := "http://" + nodes.node[i].address + "/update"
-		reader := bytes.NewReader(body)
-
-		req, err := http.NewRequest("POST", u, ioutil.NopCloser(reader))
-		if err != nil {
-			log.Fatal(err)
-			fmt.Println(rw, "ERROR: Making request"+u)
-		}
-		req.Header = r.Header
-		req.URL.Scheme = strings.Map(unicode.ToLower, req.URL.Scheme)
-		client := &http.Client{}
-		resp, err := client.Do(req)
-		if err != nil {
-			log.Fatal(err)
-			fmt.Println(rw, "ERROR: Sending request"+u)
-		}
-		output = output + u + "\nStatus: " + resp.Status + "\nProtocol: " + resp.Proto + "\n\n"
-	}
-	fmt.Println(output)
-	fmt.Fprintf(rw, output)
-}
-
-//Handles new datanodes connecting
-func HandshakeHandler(rw http.ResponseWriter, r *http.Request) {
-	handshake := mux.Vars(r)["nodeAddress"]
-	AddDataNode(handshake)
-
-	fmt.Println("Handshake: " + handshake)
-}
-
-//Func for multicasting id of file to delete to nodes
-func FileDeleteHandler(rw http.ResponseWriter, r *http.Request) {
-	fmt.Println("master ok")
-	id := mux.Vars(r)["id"] //Get file id from request path
-	output := ""
-
-	// Loop over all data nodes
-	for i := 0; i < len(nodes.node); i++ {
-		u := "http://" + nodes.node[i].address + "/delete/" + id //Specific url for every node
-
-		req, err := http.NewRequest("DELETE", u, nil) //Create new request
-		if err != nil {
-			log.Fatal(err)
-			fmt.Println(rw, "ERROR: Making request"+u)
-		}
-		req.Header = r.Header
-		req.URL.Scheme = strings.Map(unicode.ToLower, req.URL.Scheme)
-
-		client := &http.Client{}
-		resp, err := client.Do(req) //Send request, get response
-		if err != nil {
-			log.Fatal(err)
-			fmt.Println(rw, "ERROR: Sending request"+u)
-		}
-		output = output + u + "\nStatus: " + resp.Status + "\nProtocol: " + resp.Proto + "\n\n" //Output string
-	}
-	fmt.Println(output)
-	fmt.Fprintf(rw, output)
-}
-
-func NotifyRouter() {
-	masterAddress := "localhost:" + os.Getenv("PORT")
-
-	url := "http://" + routerAddress + "/handshake/" + masterAddress
-	r, err := http.NewRequest("POST", url, nil)
-	if err != nil {
-		log.Fatal(err)
-		fmt.Printf("ERROR: Making request" + url)
-	}
-
-	client := &http.Client{}
-	resp, err := client.Do(r)
-
-	if err != nil {
-		log.Fatal(err)
-		fmt.Printf("ERROR: Sending request" + url)
-	}
-	output := url + "\nStatus: " + resp.Status + "\nProtocol: " + resp.Proto + "\n\n"
-
-	fmt.Println(output)
 }
 
 //func get datanode ip
